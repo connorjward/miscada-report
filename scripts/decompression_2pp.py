7847a989ef97b447154a374073fe6ea2cc43b662
Connor Ward <cward.contrib@fastmail.com>
2020-08-13 13:19:40 +0100

    Add 2pp and batch scripts, figs and data

---
+++ scripts/decompression_2pp.py
@@ -0,0 +1,50 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import pandas as pd
+
+import myutils.mpl
+
+
+FIN = "../data/decompression_2pp/particles.csv"
+FOUT_PDF = "../figures/decompression_2pp.pdf"
+FOUT_PGF = "../figures/decompression_2pp.pgf"
+FIG_WIDTH = 0.45 * 681.159  # in pts
+
+CNAMES = ["CaO","FeO","MgO","SiO2"]
+
+
+mpl.use('pgf')  # this disables plt.show()
+mpl.rcParams.update({'font.family': 'serif',
+    'text.usetex': True,
+    'axes.labelsize': 8,
+    'font.size': 8,
+    'legend.fontsize': 8,
+    'xtick.labelsize': 8,
+    'ytick.labelsize': 8 })
+
+df = pd.read_csv(FIN)
+
+fig = plt.figure(dpi=200, figsize=myutils.mpl.figsize_from_width(FIG_WIDTH))
+ax1 = fig.add_axes((0.1, 0.5, 0.8, 0.4))
+ax2 = fig.add_axes((0.1, 0.1, 0.8, 0.3), sharex=ax1)
+
+# Remove the last point where the particle vanishes.
+df = df[:-1]
+
+for cname in CNAMES:
+    ax1.plot(
+        "Time",
+        "avg(melt_{})".format(cname), 
+        data=df,
+        label=cname
+    )
+ax1.legend(frameon=False)
+ax1.set_ylabel("Melt composition (mol)")
+
+ax2.plot("Time", "avg(porosity)", data=df)
+ax2.set_xlabel("Time (yr)")
+ax2.set_ylabel("Melt amount (mol)")
+
+plt.savefig(FOUT_PDF, bbox_inches="tight")
+plt.savefig(FOUT_PGF, bbox_inches="tight")
+
--- scripts/decompression_batch.py
+++ scripts/decompression_batch.py
@@ -1,26 +1,50 @@
-import sys
-
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import pandas as pd
 
+import myutils.mpl
 
-fname = sys.argv[1]
 
-df = pd.read_csv(fname)
+FIN = "../data/decompression_batch/particles.csv"
+FOUT_PDF = "../figures/decompression_batch.pdf"
+FOUT_PGF = "../figures/decompression_batch.pgf"
+FIG_WIDTH = 0.45 * 681.159  # in pts
 
-fig,ax = plt.subplots()
-for cname in ["CaO","FeO","MgO","SiO2"]:
-    ax.plot(
-        "Time", 
-        "avg(composition liquid {})".format(cname), 
+CNAMES = ["CaO","FeO","MgO","SiO2"]
+
+
+mpl.use('pgf')  # this disables plt.show()
+mpl.rcParams.update({'font.family': 'serif',
+    'text.usetex': True,
+    'axes.labelsize': 8,
+    'font.size': 8,
+    'legend.fontsize': 8,
+    'xtick.labelsize': 8,
+    'ytick.labelsize': 8 })
+
+df = pd.read_csv(FIN)
+
+fig = plt.figure(dpi=200, figsize=myutils.mpl.figsize_from_width(FIG_WIDTH))
+ax1 = fig.add_axes((0.1, 0.5, 0.8, 0.4))
+ax2 = fig.add_axes((0.1, 0.1, 0.8, 0.3), sharex=ax1)
+
+# Remove the last point where the particle vanishes.
+df = df[:-1]
+
+for cname in CNAMES:
+    ax1.plot(
+        "Time",
+        "avg(liquid composition {})".format(cname), 
         data=df,
         label=cname
     )
+ax1.legend(frameon=False)
+ax1.set_ylabel("Melt composition (mol)")
 
-ax2 = ax.twinx()
-ax2.plot("Time", "avg(n moles liquid)", data=df)
+ax2.plot("Time", "avg(liquid n moles)", data=df)
+ax2.set_xlabel("Time (yr)")
+ax2.set_ylabel("Melt amount (mol)")
 
+plt.savefig(FOUT_PDF, bbox_inches="tight")
+plt.savefig(FOUT_PGF, bbox_inches="tight")
 
-fig.legend()
-
-plt.show()
