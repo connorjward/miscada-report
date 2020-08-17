f8282b06903a76c7dfe4bf5829a198580d86bd2c
Connor Ward <cward.contrib@fastmail.com>
2020-08-17 15:14:09 +0100

    Improve decompression plots

--- scripts/decompression_batch.py
+++ scripts/decompression_batch.py
@@ -1,5 +1,6 @@
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
 import myutils.mpl
@@ -11,6 +12,7 @@ FOUT_PGF = "../figures/decompression_batch.pgf"
 FIG_WIDTH = 0.45 * 681.159  # in pts
 
 CNAMES = ["CaO","FeO","MgO","SiO2"]
+COLUMNS = ["avg(liquid composition {})".format(cname) for cname in CNAMES]
 
 
 mpl.use('pgf')  # this disables plt.show()
@@ -24,26 +26,31 @@ mpl.rcParams.update({'font.family': 'serif',
 
 df = pd.read_csv(FIN)
 
-fig = plt.figure(dpi=200, figsize=myutils.mpl.figsize_from_width(FIG_WIDTH))
-ax1 = fig.add_axes((0.1, 0.5, 0.8, 0.4))
-ax2 = fig.add_axes((0.1, 0.1, 0.8, 0.3), sharex=ax1)
+fig,ax = plt.subplots(dpi=200, figsize=myutils.mpl.figsize_from_width(FIG_WIDTH))
 
-# Remove the last point where the particle vanishes.
-df = df[:-1]
+# First plot the melt amount.
+ax.plot("Time", "avg(liquid n moles)", data=df, 
+        color="black", label="_nolegend_")
 
-for cname in CNAMES:
-    ax1.plot(
-        "Time",
-        "avg(liquid composition {})".format(cname), 
-        data=df,
-        label=cname
-    )
-ax1.legend(frameon=False)
-ax1.set_ylabel("Melt composition (mol)")
+xs = []
+yss = []
+for i,row in df.iterrows():
+    x = row["Time"]
 
-ax2.plot("Time", "avg(liquid n moles)", data=df)
-ax2.set_xlabel("Time (yr)")
-ax2.set_ylabel("Melt amount (mol)")
+    if sum(row[COLUMNS]) > 0:
+        ys = row[COLUMNS] / sum(row[COLUMNS]) * row["avg(liquid n moles)"]
+    else:
+        ys = [0.0 for _ in row[COLUMNS]]
+
+    xs.append(x)
+    yss.append(ys)
+
+ax.stackplot(np.array(xs), np.array(yss).swapaxes(0, 1), labels=CNAMES)
+
+ax.legend(frameon=False)
+ax.set_xlabel("Time (yr)")
+ax.set_ylabel("Melt amount (mol)")
+ax.set_xlim(1, 100000)
 
 plt.savefig(FOUT_PDF, bbox_inches="tight")
 plt.savefig(FOUT_PGF, bbox_inches="tight")
