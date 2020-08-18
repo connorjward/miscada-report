6167927691801e2906e7455a5f710ac1aea051bb
Connor Ward <cward.contrib@fastmail.com>
2020-08-18 18:06:58 +0100

    Update load balancing plots

--- scripts/load_balancing/plot.py
+++ scripts/load_balancing/plot.py
@@ -1,8 +1,58 @@
-# import pandas as pd
 import re
 
-with open("2pp_statistics") as f:
-    match = re.findall("# \d+: (.*)", f.read())
-    
-print(match)
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import pandas as pd
+
+import myutils.mpl
+
+
+FIN_BATCH = "batch_statistics"
+FIN_2PP = "2pp_statistics"
+
+FOUT_PDF = "../../figures/load_balancing.pdf"
+FOUT_PGF = "../../figures/load_balancing.pgf"
+
+FIG_WIDTH = 0.45 * 681.159  # in pts
+
+
+def read_statistics(fname):
+    with open(fname) as f:
+        cols = re.findall("# \d+: (.*)", f.read())
+        
+    return pd.read_table(fname, names=cols, comment="#", delim_whitespace=True)
+
+
+mpl.use('pgf')  # this disables plt.show()
+mpl.rcParams.update({
+    'font.family': 'serif', 
+    'text.usetex': True,
+    'axes.labelsize': 8, 'font.size': 8,
+    'legend.fontsize': 8,
+    'xtick.labelsize': 8,
+    'ytick.labelsize': 8 
+})
+
+fig,ax = plt.subplots(figsize=myutils.mpl.figsize_from_width(FIG_WIDTH),
+                      dpi=200)
+
+df = read_statistics(FIN_BATCH)
+xs = df["Time (years)"]
+ys = ((df["Maximal particles per process"] 
+     - df["Minimal particles per process"])
+     / df["Number of advected particles"])
+ax.plot(xs, ys, label="Particle plugin")
+
+df = read_statistics(FIN_2PP)
+xs = df["Time (years)"]
+ys = ((df["Maximal cells per process"] - df["Minimal cells per process"])
+        / df["Number of mesh cells"])
+ax.plot(xs, ys, label="Material model")
+
+ax.set_xlabel("Time (years)")
+ax.set_ylabel("Normalised range")
+ax.legend(frameon=False)
+
+plt.savefig(FOUT_PDF, bbox_inches="tight")
+plt.savefig(FOUT_PGF, bbox_inches="tight")
 
