a6a83b8a64e5fd92af54e9bd53b45e3f4548a258
Connor Ward <cward.contrib@fastmail.com>
2020-08-20 14:51:48 +0100

    Add cache usage material model plots

--- scripts/cache_usage/material_model/plot.py
+++ scripts/cache_usage/material_model/plot.py
@@ -2,32 +2,25 @@ import re
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
 import myutils.mpl
 
 
-# FINS_BATCH = ???
-# FINS_2PP = ???
+CACHE_TOLS = ["1e-{}".format(n) for n in range(2, 6)]
 
-FOUT_PDF = "../../figures/weak_scaling/figure.pdf"
-FOUT_PGF = "../../figures/weak_scaling/figure.pgf"
+FOUT_PDF = "../../../figures/cache_usage/material_model.pdf"
+FOUT_PGF = "../../../figures/cache_usage/material_model.pgf"
 
 FIG_WIDTH = 0.45 * 681.159  # in pts
 
 
 def parse_file(fname):
-    print(fname)
     with open(fname) as f:
-        s = f.read()
-
-    match1 = re.search("running with (\d+) MPI process", s)
-    match2 = re.search("Total wallclock time elapsed since start\s+\|\s+(\S+)s", s)
-
-    nproc = int(match1.group(1))
-    time = float(match2.group(1))
+        cols = re.findall("# \d+: (.*)", f.read())
         
-    return nproc,time
+    return pd.read_table(fname, names=cols, comment="#", delim_whitespace=True)
 
 
 mpl.use('pgf')  # this disables plt.show()
@@ -43,24 +36,15 @@ mpl.rcParams.update({
 fig,ax = plt.subplots(figsize=myutils.mpl.figsize_from_width(FIG_WIDTH),
                       dpi=200)
 
-xs = []
-ys = []
-for nproc in range(1, 13):
-    x,y = parse_file("out/batch{}.txt".format(nproc))
-    xs.append(x)
-    ys.append(y)
-ax.plot(xs, ys, label="Particle property")
+for tol in CACHE_TOLS:
+    df = parse_file("data/{}.txt".format(tol))
+    xs = df["Time (years)"]
+    ys = df["Cache hit rate"]
 
-xs = []
-ys = []
-for nproc in range(1, 13):
-    x,y = parse_file("out/2pp{}.txt".format(nproc))
-    xs.append(x)
-    ys.append(y)
-ax.plot(xs, ys, label="Material model")
+    ax.plot(xs, ys, label=tol)
 
-ax.set_xlabel("Number of processors")
-ax.set_ylabel("Runtime (s)")
+ax.set_xlabel("Time (years)")
+ax.set_ylabel("Hit rate")
 ax.legend(frameon=False)
 
 plt.savefig(FOUT_PDF, bbox_inches="tight")
